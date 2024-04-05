f60fe35cfcfca21f5317cab6e84b1c78e12633b6
Cesar <guto_montaldi@yahoo.com.br>
2024-04-05 15:13:25 -0300

    Renderizando novos itens

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -34,4 +34,20 @@ export class ListaDeCompraService {
   getListaDeCompra(){
     return this.listaDeCompra;
   }
+
+  criarItem(nomeItem: string) {
+    const id = this.listaDeCompra.length + 1
+    const item : Item = {
+      id: id,
+      nome: nomeItem,
+      data: new Date().toLocaleString('pt-BR'),
+      comprado: false
+    }
+    return item;
+  }
+
+  adicionarItemLista(nomeItem: string) {
+    const item = this.criarItem(nomeItem)
+    this.listaDeCompra.push(item)
+  }
 }
